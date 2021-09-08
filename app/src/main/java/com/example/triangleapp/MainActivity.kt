da1e713894e57eaccf0bfd4af388c120aadef122
Ceaser Tenorio <ceaser.tenorio@student.csulb.edu>
2021-09-08 02:55:08 -0700

    Exception Handled for empty string, less than three sides and more than 3 sides

--- app/src/main/java/com/example/triangleapp/MainActivity.kt
+++ app/src/main/java/com/example/triangleapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,37 +23,60 @@ class MainActivity : AppCompatActivity() {
             //val num1_1 = num1.text.toString();
             //val num2_2 = num2.text.toString().toInt();
             //val num3_3 = num3.text.toString().toInt();
-
-            val UserInputString = inputValue.text.toString();
-            val UserInputArray = UserInputString.split(',').toTypedArray();
-
-
-            val num1_1 = UserInputArray[0].toDouble();
-            val num2_2 = UserInputArray[1].toDouble();
-            val num3_3 = UserInputArray[2].toDouble();
-
-
-
-            if(num1_1 != num2_2 && num1_1 != num3_3 && num2_2 != num3_3)
+            var UserInputString: String = " ";
+            var UserInputArray = ArrayList<String>();
+            var num1_1: Double = 1.0;
+            var num2_2: Double = 1.0;
+            var num3_3: Double = 1.0;
+            try
             {
-                answer.setText("[" + UserInputString + "] = scalene")
+                UserInputString = inputValue.text.toString();
+                UserInputArray = UserInputString.split(',') as ArrayList<String>;
+
             }
-            else if(((num1_1 == num2_2) || (num1_1 == num3_3) || (num2_2 == num3_3)) &&
-                ((num1_1 != num2_2) || (num1_1 != num3_3) || (num2_2 != num3_3)))
+            catch (e: ClassCastException)
             {
-                answer.setText("[" + UserInputString + "] = isosceles")
-            }
-            else if ((num3_3 == num1_1) && (num3_3 == num2_2))
-            {
-                answer.setText("[" + UserInputString + "] = equilateral")
-            }
-            else
-            {
-                answer.setText("This Triangle is NOT Scalene, Isosceles, or Equilateral!")
+                Toast.makeText(applicationContext, "No input detected, must enter 3 numbers separated by comma.  Example: 15, 23, 27", Toast.LENGTH_LONG).show();
             }
 
+            //val UserInputArray = UserInputString.split(',').toTypedArray();
+
+            try
+            {
+                if(UserInputArray.size > 3)
+                {
+                    Toast.makeText(applicationContext, "More than 3 sides detected.  Must enter 3 numbers separted by comma.  Example: 15, 23, 27", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    num1_1 = UserInputArray[0].toDouble();
+                    num2_2 = UserInputArray[1].toDouble();
+                    num3_3 = UserInputArray[2].toDouble();
+                    if(num1_1 != num2_2 && num1_1 != num3_3 && num2_2 != num3_3)
+                    {
+                        answer.setText("[" + UserInputString + "] = scalene")
+                    }
+                    else if(((num1_1 == num2_2) || (num1_1 == num3_3) || (num2_2 == num3_3)) &&
+                        ((num1_1 != num2_2) || (num1_1 != num3_3) || (num2_2 != num3_3)))
+                    {
+                        answer.setText("[" + UserInputString + "] = isosceles")
+                    }
+                    else if ((num3_3 == num1_1) && (num3_3 == num2_2))
+                    {
+                        answer.setText("[" + UserInputString + "] = equilateral")
+                    }
+                    else
+                    {
+                        answer.setText("This Triangle is NOT Scalene, Isosceles, or Equilateral!")
+                    }
+                }
+            }
+            catch(e: IndexOutOfBoundsException)
+            {
+                if(UserInputArray.size < 3)
+                {
+                    Toast.makeText(applicationContext, "Less than 3 sides detected.  Must enter 3 numbers separted by comma.  Example: 15, 23, 27", Toast.LENGTH_LONG).show();
+                }
+            }
         }
-
-
     }
 }

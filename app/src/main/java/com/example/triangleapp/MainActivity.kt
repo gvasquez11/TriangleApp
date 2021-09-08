dd853cf68ddf05606ddcc2a4ac24395238d84b9e
Ceaser Tenorio <ceaser.tenorio@student.csulb.edu>
2021-09-08 15:48:14 -0700

    Input only accepted between [1.0, 100.0] inclusive, displays error message if not with in range

--- app/src/main/java/com/example/triangleapp/MainActivity.kt
+++ app/src/main/java/com/example/triangleapp/MainActivity.kt
@@ -14,17 +14,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
 
+        // Creation and initialization of Variables and values
         val inputValue = findViewById(R.id.UserInput) as EditText;
-
         val answer = findViewById(R.id.Result) as TextView;
         val analyzeAnswer = findViewById(R.id.AnalyzeButton) as Button;
+        var UserInputString: String = " ";
+        var UserInputArray = ArrayList<String>();
+        var num1_1: Double = 1.0;
+        var num2_2: Double = 1.0;
+        var num3_3: Double = 1.0;
+        var limits: Boolean = true;
 
         analyzeAnswer.setOnClickListener{
-            var UserInputString: String = " ";
-            var UserInputArray = ArrayList<String>();
-            var num1_1: Double = 1.0;
-            var num2_2: Double = 1.0;
-            var num3_3: Double = 1.0;
+
 
             try
             {
@@ -48,22 +50,25 @@ class MainActivity : AppCompatActivity() {
                     num1_1 = UserInputArray[0].toDouble();
                     num2_2 = UserInputArray[1].toDouble();
                     num3_3 = UserInputArray[2].toDouble();
-                    if(num1_1 != num2_2 && num1_1 != num3_3 && num2_2 != num3_3)
-                    {
-                        answer.setText("[" + UserInputString + "] = scalene")
-                    }
-                    else if(((num1_1 == num2_2) || (num1_1 == num3_3) || (num2_2 == num3_3)) &&
-                        ((num1_1 != num2_2) || (num1_1 != num3_3) || (num2_2 != num3_3)))
-                    {
-                        answer.setText("[" + UserInputString + "] = isosceles")
-                    }
-                    else if ((num3_3 == num1_1) && (num3_3 == num2_2))
-                    {
-                        answer.setText("[" + UserInputString + "] = equilateral")
-                    }
-                    else
-                    {
-                        answer.setText("This Triangle is NOT Scalene, Isosceles, or Equilateral!")
+                    limits = inLimits(UserInputArray);
+                    if(limits){
+                        if(num1_1 != num2_2 && num1_1 != num3_3 && num2_2 != num3_3)
+                        {
+                            answer.setText("[" + UserInputString + "] = scalene")
+                        }
+                        else if(((num1_1 == num2_2) || (num1_1 == num3_3) || (num2_2 == num3_3)) &&
+                            ((num1_1 != num2_2) || (num1_1 != num3_3) || (num2_2 != num3_3)))
+                        {
+                            answer.setText("[" + UserInputString + "] = isosceles")
+                        }
+                        else if ((num3_3 == num1_1) && (num3_3 == num2_2))
+                        {
+                            answer.setText("[" + UserInputString + "] = equilateral")
+                        }
+                        else
+                        {
+                            answer.setText("This Triangle is NOT Scalene, Isosceles, or Equilateral!")
+                        }
                     }
                 }
             }
@@ -79,5 +84,23 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, "Number Format Exception, multiple points or missing comma", Toast.LENGTH_LONG).show();
             }
         }
+    }
+    fun inLimits (values: List<String>): Boolean {
+
+        for (i in values.indices )
+        {
+            if(values[i].toDouble() < 1.0){
+                Toast.makeText(applicationContext, "Value Out of Range, Must be greater or equal to 1.0", Toast.LENGTH_LONG).show();
+                return false;
+                break;
+            }
+            else if (values[i].toDouble() > 100.0)
+            {
+                Toast.makeText(applicationContext, "Value Out of Range, Must be less than or equal to 100.0", Toast.LENGTH_LONG).show();
+                return false;
+                break;
+            }
+        }
+        return true;
     }
 }

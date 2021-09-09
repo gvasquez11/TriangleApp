ffdcd25c21c47bba0d7f49a65c7934a6812bc057
gvasquez11 <vasquez.greg16@gmail.com>
2021-09-08 22:45:40 -0700

    User is now able to exit with button and zero

--- app/src/main/java/com/example/triangleapp/MainActivity.kt
+++ app/src/main/java/com/example/triangleapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.triangleapp;
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
@@ -14,10 +16,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main);
 
+
         // Creation and initialization of Variables and values
         val inputValue = findViewById(R.id.UserInput) as EditText;
         val answer = findViewById(R.id.Result) as TextView;
         val analyzeAnswer = findViewById(R.id.AnalyzeButton) as Button;
+        val exitB = findViewById(R.id.button2) as Button;
         var UserInputString: String = " ";
         var UserInputArray = ArrayList<String>();
         var num1_1: Double = 1.0;
@@ -25,81 +29,101 @@ class MainActivity : AppCompatActivity() {
         var num3_3: Double = 1.0;
         var limits: Boolean = true;
         var OutputStr1: String = " ";
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Exit Confirmation");
+        builder.setMessage("Are you sure you want to exit?");
+        builder.setPositiveButton("Yes", { dialogInterface: DialogInterface, i: Int -> finish()})
+        builder.setNegativeButton("No", { dialogInterface: DialogInterface, i: Int -> })
 
-        analyzeAnswer.setOnClickListener{
+        exitB.setOnClickListener{
+            builder.show()
+        }
 
+        analyzeAnswer.setOnClickListener {
 
-            try
-            {
-                UserInputString = inputValue.text.toString();
-                UserInputArray = UserInputString.split(',') as ArrayList<String>;
+            if (inputValue.text.toString() == "0") {
+                builder.show()
+            } else {
+                try {
+                    UserInputString = inputValue.text.toString();
+                    UserInputArray = UserInputString.split(',') as ArrayList<String>;
 
-            }
-            catch (e: ClassCastException)
-            {
-                Toast.makeText(applicationContext, "Invalid input detected, must enter 3 numbers separated by comma.  Example: 15, 23, 27", Toast.LENGTH_LONG).show();
-            }
-
-
-            try
-            {
-                if(UserInputArray.size > 3)
-                {
-                    Toast.makeText(applicationContext, "More than 3 sides detected.  Must enter 3 numbers separated by comma.  Example: 15, 23, 27", Toast.LENGTH_LONG).show();
+                } catch (e: ClassCastException) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Invalid input detected, must enter 3 numbers separated by comma.  Example: 15, 23, 27",
+                        Toast.LENGTH_LONG
+                    ).show();
                 }
-                else {
-                    num1_1 = UserInputArray[0].toDouble();
-                    num2_2 = UserInputArray[1].toDouble();
-                    num3_3 = UserInputArray[2].toDouble();
-                    limits = inLimits(UserInputArray);
-                    OutputStr1 = "[" + UserInputArray[0].trim() +", " + UserInputArray[1].trim()+ ", "+ UserInputArray[2].trim() + "] = ";
-                    if(limits){
-                        if ((num1_1 + num2_2 < num3_3) ||
+
+
+                try {
+                    if (UserInputArray.size > 3) {
+                        Toast.makeText(
+                            applicationContext,
+                            "More than 3 sides detected.  Must enter 3 numbers separated by comma.  Example: 15, 23, 27",
+                            Toast.LENGTH_LONG
+                        ).show();
+                    } else {
+                        num1_1 = UserInputArray[0].toDouble();
+                        num2_2 = UserInputArray[1].toDouble();
+                        num3_3 = UserInputArray[2].toDouble();
+                        limits = inLimits(UserInputArray);
+                        OutputStr1 =
+                            "[" + UserInputArray[0].trim() + ", " + UserInputArray[1].trim() + ", " + UserInputArray[2].trim() + "] = ";
+                        if (limits) {
+                            if ((num1_1 + num2_2 < num3_3) ||
                                 (num1_1 + num3_3 < num2_2) ||
-                                (num2_2 + num3_3 < num1_1))
-                        {
-                            Toast.makeText(applicationContext, "The three values entered can't make a triangle due to the Triangle Inequality Theorem", Toast.LENGTH_LONG).show();
+                                (num2_2 + num3_3 < num1_1)
+                            ) {
+                                Toast.makeText(
+                                    applicationContext,
+                                    "The three values entered can't make a triangle due to the Triangle Inequality Theorem",
+                                    Toast.LENGTH_LONG
+                                ).show();
 
-                        }
-
-                        else if(num1_1 != num2_2 && num1_1 != num3_3 && num2_2 != num3_3)
-                        {
-                            answer.setText(OutputStr1 + "scalene")
-                        }
-                        else if(((num1_1 == num2_2) || (num1_1 == num3_3) || (num2_2 == num3_3)) &&
-                            ((num1_1 != num2_2) || (num1_1 != num3_3) || (num2_2 != num3_3)))
-                        {
-                            answer.setText(OutputStr1 + "isosceles")
-                        }
-                        else if ((num3_3 == num1_1) && (num3_3 == num2_2))
-                        {
-                            answer.setText(OutputStr1 + "equilateral")
-                        }
-                        else
-                        {
-                            answer.setText("If our logic is correct, this error is mathematically impossible to occur.")
+                            } else if (num1_1 != num2_2 && num1_1 != num3_3 && num2_2 != num3_3) {
+                                answer.setText(OutputStr1 + "scalene")
+                            } else if (((num1_1 == num2_2) || (num1_1 == num3_3) || (num2_2 == num3_3)) &&
+                                ((num1_1 != num2_2) || (num1_1 != num3_3) || (num2_2 != num3_3))
+                            ) {
+                                answer.setText(OutputStr1 + "isosceles")
+                            } else if ((num3_3 == num1_1) && (num3_3 == num2_2)) {
+                                answer.setText(OutputStr1 + "equilateral")
+                            } else {
+                                answer.setText("If our logic is correct, this error is mathematically impossible to occur.")
+                            }
                         }
                     }
+                } catch (e: IndexOutOfBoundsException) {
+                    if (UserInputArray.size == 1 && UserInputArray[0].toInt() == 0)
+                        finish()
+                    else if (UserInputArray.size < 3) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Less than 3 sides detected.  Must enter 3 numbers separated by comma.  Example: 15, 23, 27",
+                            Toast.LENGTH_LONG
+                        ).show();
+                    }
+                } catch (e: NumberFormatException) {
+                    Toast.makeText(
+                        applicationContext,
+                        "Number Format Exception, multiple points or missing comma",
+                        Toast.LENGTH_LONG
+                    ).show();
                 }
-            }
-            catch(e: IndexOutOfBoundsException)
-            {
-                if(UserInputArray.size < 3)
-                {
-                    Toast.makeText(applicationContext, "Less than 3 sides detected.  Must enter 3 numbers separated by comma.  Example: 15, 23, 27", Toast.LENGTH_LONG).show();
-                }
-            }
-            catch(e:NumberFormatException)
-            {
-                Toast.makeText(applicationContext, "Number Format Exception, multiple points or missing comma", Toast.LENGTH_LONG).show();
             }
         }
     }
+
+
     fun inLimits (values: List<String>): Boolean {
 
         for (i in values.indices )
         {
-            if(values[i].toDouble() < 1.0){
+            if(values[i].toInt() == 0)
+                finish();
+            else if(values[i].toDouble() < 1.0){
                 Toast.makeText(applicationContext, "Value Out of Range, Must be greater or equal to 1.0", Toast.LENGTH_LONG).show();
                 return false;
                 break;
